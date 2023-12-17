9749ee7e5b9e3db8f0ce359ef7fa356ec54ed8eb
jjuuuunnii <jake991110@naver.com>
2023-12-18 00:17:20 +0900

    Feat: 스프링프로젝트 생성

---
+++ MyProject/red-nosed-donggukThon/app/src/main/java/rednosed/app/RedNosedAppApplication.java
@@ -0,0 +1,14 @@
+package rednosed.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedNosedAppApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedNosedAppApplication.class, args);
+	}
+
+}
---
+++ MyProject/red-nosed-donggukThon/app/src/test/java/rednosed/app/RedNosedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package rednosed.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedNosedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
