19564f528b98cb3dfe0ef8f3730a2c64cde080a6
jjuuuunnii <jake991110@naver.com>
2023-12-18 23:43:28 +0900

    Feat: 3-2. 우표만들기(캔버스: 소켓) 완료

--- src/main/java/rednosed/app/controller/StompController.java
+++ src/main/java/rednosed/app/controller/StompController.java
@@ -6,21 +6,29 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import rednosed.app.dto.common.ResponseDto;
 import rednosed.app.dto.request.PixelInfoDto;
+import rednosed.app.exception.custom.CustomException;
 import rednosed.app.service.PixelService;
 
 @RestController
 @RequiredArgsConstructor
-public class SocketController {
+public class StompController {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final PixelService pixelService;
 
+    //3-2. 우표만들기(캔버스: 소켓)
     @MessageMapping("/canvas/{canvasId}")
     public void sendCanvasData(
             @DestinationVariable String canvasId,
             @RequestBody PixelInfoDto pixelInfoDto)
     {
-
+        try {
+            pixelService.saveCanvasData(canvasId, pixelInfoDto);
+            simpMessagingTemplate.convertAndSend("/subscribe/canvas/" + canvasId, ResponseDto.ok(pixelInfoDto));
+        } catch (CustomException e) {
+            simpMessagingTemplate.convertAndSend("/subscribe/canvas/", ResponseDto.fail(e));
+        }
     }
 }
--- src/main/java/rednosed/app/service/PixelService.java
+++ src/main/java/rednosed/app/service/PixelService.java
@@ -2,6 +2,10 @@ package rednosed.app.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import rednosed.app.domain.nosql.Pixel;
+import rednosed.app.dto.request.PixelInfoDto;
+import rednosed.app.dto.type.ErrorCode;
+import rednosed.app.exception.custom.CustomException;
 import rednosed.app.repository.nosql.PixelRepository;
 
 @Service
@@ -10,4 +14,9 @@ public class PixelService {
 
     private final PixelRepository pixelRepository;
 
+    public void saveCanvasData(String canvasClientId, PixelInfoDto pixelInfoDto) {
+        Pixel pixel = pixelRepository.findByCanvasClientId(canvasClientId)
+                .orElseThrow(() -> new CustomException(ErrorCode.PIXEL_NOT_FOUND));
+        pixel.updateColor(pixelInfoDto.xCoordinate(), pixelInfoDto.yCoordinate(), pixelInfoDto.color());
+    }
 }
