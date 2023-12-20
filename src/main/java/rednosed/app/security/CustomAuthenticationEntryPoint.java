1f008c93a900c64e8203ef602cd5648cc14fad01
jjuuuunnii <jake991110@naver.com>
2023-12-20 10:03:09 +0900

    Feat: Commece 수정

--- src/main/java/rednosed/app/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/rednosed/app/security/CustomAuthenticationEntryPoint.java
@@ -1,11 +1,15 @@
 package rednosed.app.security;
 
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import net.minidev.json.JSONValue;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import rednosed.app.dto.type.ErrorCode;
@@ -22,11 +26,37 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         Map<String, Object> result = new HashMap<>();
-        ErrorCode errorCode = request.getAttribute("exception") == null ?
-                ErrorCode.NOT_FOUND_END_POINT : (ErrorCode) request.getAttribute("exception");
-        setResponseMap(result, errorCode.getMessage());
+        HttpStatus status;
+        if (authException.getCause() instanceof InsufficientAuthenticationException) {
+            log.error("FilterException throw MalformedJwtException Exception");
+            setResponseMap(result,ErrorCode.TOKEN_MALFORMED_ERROR.getMessage());
+            status = ErrorCode.TOKEN_MALFORMED_ERROR.getHttpStatus();
+        } else if (authException.getCause() instanceof IllegalArgumentException) {
+            log.error("FilterException throw IllegalArgumentException Exception");
+            setResponseMap(result,ErrorCode.TOKEN_TYPE_ERROR.getMessage());
+            status = ErrorCode.TOKEN_TYPE_ERROR.getHttpStatus();
 
-        HttpStatus status = errorCode.getHttpStatus();
+        } else if (authException.getCause() instanceof TokenExpiredException){
+            log.error("FilterException throw ExpiredJwtException Exception");
+            setResponseMap(result,ErrorCode.EXPIRED_TOKEN_ERROR.getMessage());
+            status = ErrorCode.EXPIRED_TOKEN_ERROR.getHttpStatus();
+
+        } else if (authException.getCause() instanceof UnsupportedJwtException) {
+            log.error("FilterException throw UnsupportedJwtException Exception");
+            setResponseMap(result,ErrorCode.TOKEN_UNSUPPORTED_ERROR.getMessage());
+            status = ErrorCode.TOKEN_UNSUPPORTED_ERROR.getHttpStatus();
+
+        } else if (authException.getCause() instanceof JwtException) {
+            log.error("FilterException throw JwtException Exception");
+            setResponseMap(result,ErrorCode.TOKEN_UNKNOWN_ERROR.getMessage());
+            status = ErrorCode.TOKEN_UNKNOWN_ERROR.getHttpStatus();
+
+        } else {
+            log.error("FilterException throw Exception Exception");
+            setResponseMap(result,ErrorCode.INTERNAL_SERVER_ERROR.getMessage());
+            status = ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus();
+        }
+
         sendResponse(response, result, status);
         log.error("Authentication error: {}", authException.getMessage(), authException);
     }
