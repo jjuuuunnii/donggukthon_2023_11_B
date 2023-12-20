355407c4f22e4fc3377b3265f4e24bcde6de8f87
jjuuuunnii <jake991110@naver.com>
2023-12-20 16:36:38 +0900

    test

--- src/main/java/rednosed/app/contrant/Constants.java
+++ src/main/java/rednosed/app/contrant/Constants.java
@@ -6,7 +6,8 @@ public class Constants {
 
     public static List<String> NO_NEED_AUTH_URLS = List.of(
             "/login/oauth2/code/kakao",
-            "/oauth2/authorization/kakao"
+            "/oauth2/authorization/kakao",
+            "/**"
     );
 
     public static final long TOTAL_CANVAS_MAKING_TIME = 600;
