640ee339b08be6463d5ca0891b9c43db63e71db6
jjuuuunnii <129721531+jjuuuunnii@users.noreply.github.com>
2024-02-13 02:31:05 +0900

    Delete MyProject directory

--- MyProject/clipper/cafevery_/src/main/java/com/cafevery/annotation/UserId.java
+++
@@ -1,9 +0,0 @@
-package com.cafevery.annotation;
-
-import java.lang.annotation.*;
-
-@Documented
-@Target(ElementType.PARAMETER)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface UserId {
-}
--- MyProject/clipper/cafevery_/src/main/java/com/cafevery/repository/CafeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cafevery.repository;
-
-import com.cafevery.domain.Cafe;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CafeRepository extends JpaRepository<Cafe, Long> {
-}
